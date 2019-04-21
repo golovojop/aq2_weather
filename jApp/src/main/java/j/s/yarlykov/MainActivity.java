67d850a76f02e6a331aa476d937a1cc3975dbe78
ciscoff <ciscoff@ya.ru>
2019-04-21 16:10:04 +0300

    Добавил работу с Logcat для просмотра LifeCycle

--- jApp/src/main/java/j/s/yarlykov/MainActivity.java
+++ jApp/src/main/java/j/s/yarlykov/MainActivity.java
@@ -2,6 +2,7 @@ package j.s.yarlykov;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,46 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        logI("onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        logI("onStart");
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        logI("onStop");
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        logI("onDestroy");
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        logI("onPause");
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        logI("onResume");
+
+    }
+
+    private void logI(String message) {
+        final String tag = "jWeather";
+        Log.i(tag, this.getClass().getSimpleName() + ": " + message);
     }
 }
