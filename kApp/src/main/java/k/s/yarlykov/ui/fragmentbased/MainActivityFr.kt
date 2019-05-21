7834dc837e1ce74634bbaf8392f6ba293d4f11f0
yagray <yagray@mail.ru>
2019-05-21 19:05:45 +0300

    Стилизовал NavigationView

--- kApp/src/main/java/k/s/yarlykov/ui/fragmentbased/MainActivityFr.kt
+++ kApp/src/main/java/k/s/yarlykov/ui/fragmentbased/MainActivityFr.kt
@@ -1,20 +1,33 @@
 package k.s.yarlykov.ui.fragmentbased
 
 import android.content.res.Configuration
+import android.graphics.Color
+import android.graphics.LinearGradient
+import android.graphics.Shader
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.TextView
 import k.s.yarlykov.R
+import kotlinx.android.synthetic.main.activity_main_app_bar.*
+import kotlinx.android.synthetic.main.activity_main_drawer.*
 
 class MainActivityFr : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_fr)
+        setContentView(R.layout.activity_main_drawer)
+        setSupportActionBar(toolbarDrawer)
+
+        val tvText = navView.getHeaderView(0).findViewById<TextView>(R.id.appWeather)
+
+        val shader = LinearGradient(0f, 0f, 512f, 64f,
+                Color.WHITE, Color.BLUE, Shader.TileMode.REPEAT)
+        tvText?.paint?.shader = shader
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.main, menu)
         return true
     }
