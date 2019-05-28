80079ced4fc6c59ed5f49d80fb95a894c1d96a0c
yagray <yagray@mail.ru>
2019-05-28 20:19:03 +0300

    Добавил ProgressBar для загрузки истории

--- jApp/src/main/java/j/s/yarlykov/ui/fragmentbased/history/HistoryActivity.java
+++ jApp/src/main/java/j/s/yarlykov/ui/fragmentbased/history/HistoryActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.content.res.TypedArray;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.CollapsingToolbarLayout;
@@ -18,7 +19,11 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import java.util.concurrent.TimeUnit;
 
 import j.s.yarlykov.R;
 import j.s.yarlykov.data.provider.HistoryProvider;
@@ -42,18 +47,39 @@ public class HistoryActivity extends AppCompatActivity {
 
     RecyclerView rvHistory;
     TextView tvCity;
+    ProgressBar progressBar;
+    LoadTask loadTask;
+    LinearLayout pbContainer;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
         setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+
+        loadTask = (LoadTask)getLastNonConfigurationInstance();
+
         initViews();
+
+        if(loadTask == null) {
+            loadTask = new LoadTask();
+            loadTask.bind(this);
+            loadTask.execute();
+        }
+    }
+
+    @Override
+    public Object onRetainCustomNonConfigurationInstance() {
+        loadTask.unbind();
+        return loadTask;
     }
 
     private void initViews() {
         tvCity = findViewById(R.id.tvCity);
         tvCity.setText((String)getIntent().getSerializableExtra(EXTRA_HISTORY));
+
+        pbContainer = findViewById(R.id.pbContainer);
+        progressBar = findViewById(R.id.myProgressBar);
 
         // Установить фоновое изображение для CollapsingToolbarLayout
         TypedArray imagesBg = getResources().obtainTypedArray(R.array.historyBg);
@@ -125,5 +151,57 @@ public class HistoryActivity extends AppCompatActivity {
 
     private void saveLastCity() {
         lastCity = tvCity.getText().toString();
+    }
+
+    /**
+     * Task загрузки истории. Эмулирует долгую работу
+     */
+    static class LoadTask extends AsyncTask<Void, Integer, Integer> {
+        HistoryActivity historyActivity;
+
+        void bind(HistoryActivity activity) {
+            this.historyActivity = activity;
+        }
+
+        void unbind() {
+            historyActivity = null;
+        }
+
+        @Override
+        protected Integer doInBackground(Void... voids) {
+            int max = historyActivity.progressBar.getMax();
+
+            try {
+                for(int i = 10; i < max;) {
+                    TimeUnit.MILLISECONDS.sleep(100);
+                    publishProgress(i);
+                    i += 5;
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            historyActivity.rvHistory.setVisibility(View.GONE);
+            historyActivity.pbContainer.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected void onPostExecute(Integer integer) {
+            super.onPostExecute(integer);
+            historyActivity.pbContainer.setVisibility(View.GONE);
+            historyActivity.rvHistory.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            historyActivity.progressBar.setProgress(values[0]);
+        }
     }
 }
